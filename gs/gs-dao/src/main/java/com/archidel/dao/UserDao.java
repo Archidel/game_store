9bfbc6da96dfe52e9b9f50072a2e3733f578746d
Albert_Zarankovich <albert_zarankovich@epam.com>
2017-09-01 15:17:59 +0300

    empty project

--- gs/gs-dao/src/main/java/com/archidel/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.archidel.dao;
-
-import com.archidel.gs.bean.User;
-
-public interface UserDao {
-    User findById(int id);
-    User findByProfile(int id);
-}
--- gs/gs-service/src/main/java/com/archidel/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.archidel.service;
-
-import com.archidel.gs.bean.User;
-
-public interface UserService {
-    User findById(int id);
-    User findByProfile(int id);
-}
